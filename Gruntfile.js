66479950c004daf0c1bf6ba50bed827e38bd7396
Jeff Cantrill <jcantril@redhat.com>
2015-12-10 09:14:24 -0500

    Add tasks to allow creating a release on github

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,18 @@
 module.exports = function (grunt) {
-
+  
+  var releaseBase = 'origin-kibana-';
+  var releaseTag = 'v' + grunt.file.readJSON('package.json').version;
+  var releaseFile = releaseBase + releaseTag  + ".tgz";
+    
   grunt.initConfig({
     
-    lessFiles: [
-      'lib/styles/main.less'
-    ],
-
     pkg: grunt.file.readJSON('package.json'),
-
     less: {
       production: {
         paths : [
           "lib/components/patternfly/fonts"
         ],
-        src: '<%= lessFiles %>',
+        src: '**/main.less',
         expand: true,
         ext: '.css',
         options: {
@@ -22,23 +21,75 @@ module.exports = function (grunt) {
       }
     },
     copy: {
-      options: {
-        timestamp: true
-      },
       dist: {
-        expand: true,
-        cwd: 'lib/',
-        src: ['**/*', '!**/*.js','!**/*.less'],
-        dest: 'dist/',
-        filter: 'isFile'
+        options: {
+          timestamp: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'lib/',
+            src: ['**/*', '!**/*.less','!**/fonts/**','!**/fa-fonts/**'],
+            dest: 'dist/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/patternfly/dist/fonts',
+            src: '**',
+            dest: 'dist/styles/fonts/'
+          },
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/patternfly/components/font-awesome/fonts',
+            src: '**',
+            dest: 'dist/styles/fa-fonts/'
+          }
+        ]
       }
     },
-    clean: ['dist']
+    clean: ['dist', releaseBase + '*'],
+    compress: {
+      options: {
+        pretty: true,
+        archive: releaseFile,
+        mode: 'tgz'
+      },
+      files: {
+        expand: true,
+        cwd: 'dist',
+        src: '**/*',
+        dest: '.',
+      }
+    },
+    'github-release' : {
+      files : {
+        src: [releaseFile]
+      },
+      options: {
+        repository: 'openshift/origin-kibana',
+        auth : {
+          user: process.env.GITHUB_USERNAME,
+          password: process.env.GITHUB_PASSWORD
+        },
+        release : {
+          tag_name: releaseTag,
+          name: releaseTag,
+          prerelease: false
+        }
+      }
+    }
     
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.registerTask('default', ['less','copy']);
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-github-releaser');
+
+  grunt.registerTask('default', ['clean','less','copy']);
+  grunt.registerTask('release', ['default', 'compress','github-release']);
 };
